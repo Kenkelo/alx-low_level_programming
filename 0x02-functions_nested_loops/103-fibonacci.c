70aef009a146d5a774123b1df479ccbf7f765738
Kenkelo <morenikeadedoyin2000@gmail.com>
2023-04-26 02:11:08 -0700

    sum of even valued terms

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the sum of the even-valued terms
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int n1, n2, sum, evt;
+
+	n1 = 1;
+	n2 = 2;
+	sum = evt = 0;
+
+	while (sum <= 4000000)
+	{
+		sum = n1 + n2;
+		n1 = n2;
+		n2 = sum;
+		if ((n1 % 2) == 0)
+		{
+			evt += n1;
+		}
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
