9a36dca2d578c7b288ff597f5db65c879ba1f590
Kenkelo <morenikeadedoyin2000@gmail.com>
2023-04-26 04:03:57 -0700

    even liber abacci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,17 +6,21 @@
  */
 int main(void)
 {
-	long int n1, n2, sum;
+	long int n1, n2, fn, sum;
 
 	n1 = 1;
 	n2 = 2;
-	sum = 0;
+	fn = sum = 0;
 
-	while (sum <= 4000000)
+	while (fn <= 4000000)
 	{
-		sum = n1 + n2;
-		n1 += n2;
-		n2 += sum;
+		fn = n1 + n2;
+		n1 = n2;
+		n2 = fn;
+		if ((n1 % 2) == 0)
+		{
+			sum += n1;
+		}
 	}
 	printf("%ld\n", sum);
 	return (0);
