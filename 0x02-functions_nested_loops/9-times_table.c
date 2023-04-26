912cfe13bf5361c9888ad1f3e751cc6e908bfe8f
Kenkelo <morenikeadedoyin2000@gmail.com>
2023-04-25 17:39:38 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 	{
 		_putchar('0');
 
-		for (mul = 0; mul <= 9; mul++)
+		for (mul = 1; mul <= 9; mul++)
 		{
 			_putchar(',');
 			_putchar(' ');
@@ -23,7 +23,7 @@ void times_table(void)
 			if (sum <= 9)
 				_putchar(' ');
 			else
-				_putchar((sum / 10) + '8');
+				_putchar((sum / 10) + '0');
 			_putchar((sum % 10) + '0');
 		}
 		_putchar('\n');
