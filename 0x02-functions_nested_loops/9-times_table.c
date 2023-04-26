644193fd43d5c9d7a622d957271d6f0218d7f496
Kenkelo <morenikeadedoyin2000@gmail.com>
2023-04-25 17:34:59 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -23,7 +23,7 @@ void times_table(void)
 			if (sum <= 9)
 				_putchar(' ');
 			else
-				_putchar((sum / 10) + '0');
+				_putchar((sum / 10) + '8');
 			_putchar((sum % 10) + '0');
 		}
 		_putchar('\n');
