5fa2e719141be278f72bf75d4b532d84c1e1d48e
Kenkelo <morenikeadedoyin2000@gmail.com>
2023-05-24 21:51:40 -0700

    print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
  * print_name - A function that prints a name
  * @name: string to add
  * @f: pointer to function
- * Return: nothing
+ * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
